afb012b9da17c3f8e8a491415d7bb8aa1f7d4f56
Enimoop <marine.forcioli@laposte.net>
2025-12-08 10:13:56 +0100

    fix : test ci error

--- tasks/test_accessibility.py
+++ tasks/test_accessibility.py
@@ -23,11 +23,17 @@ class AccessibilityTests(LiveServerTestCase):
 
         options = Options()
 
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+
         cls.driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
             options=options,
         )
-        cls.driver.maximize_window()
+
         cls.driver.set_script_timeout(60)
 
     @classmethod
