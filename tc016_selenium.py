47d0311082e51228184e78f6b89bead77b6a7d16
Enimoop <marine.forcioli@laposte.net>
2025-12-08 09:57:58 +0100

    fix: ruff errors

--- tc016_selenium.py
+++ tc016_selenium.py
@@ -2,11 +2,11 @@ import json
 import time
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 from tc017_selenium import RESULT_JSON
 
--- tc017_selenium.py
+++ tc017_selenium.py
@@ -3,11 +3,11 @@ import time
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--headless=new")
